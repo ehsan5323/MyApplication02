9b6ccd69980edf10e9dfd53e9ad96c13bfa35b44
Ehsan Ahmadi <ehsan5323@gmail.com>
2017-11-18 23:31:22 +0330

    commit for master2

--- app/src/main/java/com/example/ehsan/myapplication02/MainActivity.java
+++ app/src/main/java/com/example/ehsan/myapplication02/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toast.makeText(this, "masterBranch", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "ehsanBranch", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "masterBranch2", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "ehsanBranch", Toast.LENGTH_SHORT).show();
 
     }
 }
