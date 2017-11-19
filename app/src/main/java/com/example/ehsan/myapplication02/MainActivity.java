26ca6bb2171760c2aaeda3b4d4fceda2ea998686
Ehsan Ahmadi <ehsan5323@gmail.com>
2017-11-19 11:26:16 +0330

    commit masterok

--- app/src/main/java/com/example/ehsan/myapplication02/MainActivity.java
+++ app/src/main/java/com/example/ehsan/myapplication02/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ehsan.myapplication02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "masterBranch_OK", Toast.LENGTH_SHORT).show();
 //        Toast.makeText(this, "ehsanBranch3", Toast.LENGTH_SHORT).show();
+        Button btnok= (Button) findViewById(R.id.btnok);
+
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "masterok", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
